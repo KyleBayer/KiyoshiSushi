bdeb87e4be631787adf938d4254bf2189f193ace
Kyle Bayer <https://KyleBayer@github.com>
2018-11-12 10:36:45 -0500

    Test

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -5,7 +5,7 @@
         <a class="navbar-brand js-scroll-trigger" href="index.php">Kiyoshi Sushi</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
-          <i class="fa fa-bars"></i>
+          <i class="fa fa-bars">hello</i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav text-uppercase ml-auto">
