79102224b3c7c7909135018ad3c810eb0cd2e1a3
Kyle Bayer <https://KyleBayer@github.com>
2018-11-12 11:06:33 -0500

    login route

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/login', 'Auth\LoginController@credentials')->name('home');
+Route::get('/login', 'Auth\LoginController@login')->name('home');
