0083a686ce5a38ce784980809ee81b7966579201
Mahmood Sadat <said.sadat@dcmail.ca>
2018-11-17 15:05:13 -0500

    Just added a non-required email for paypal

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+@extends ('layouts.master')
+
+@section ('content')
+<header class="masthead">
+      <div class="container">
+        <div class="intro-text">
+          <div class="intro-lead-in">About Us</div>
+          <div class="intro-heading text-uppercase">The Web Developers</div>
+        </div>
+      </div>
+</header>
+
+    
+
+   <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+   <h1 class="display-4">Developers</h1><br/>
+      <p>
+        We are a group of 5 that teamed up to develop this wonderful website. We have developed anything a customer may need when they require online food service from Kiyoshi Sushi. Please email us or join our Discord for any help!
+      </p>
+   </div>
+
+@endsection
