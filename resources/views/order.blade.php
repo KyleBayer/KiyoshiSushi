2fd0bf36e46051f621f5e2a4be3f7cd5a641ff07
Mahmood Sadat <said.sadat@dcmail.ca>
2018-11-19 10:28:54 -0500

    Justaddedorderpage
    APP_NAME=Laravel
    APP_ENV=local
    APP_KEY=base64:p5yDxQtqATJJaVDxkY6ASS1t3hTa4lcFLamp5EAUrnA=
    APP_DEBUG=true
    APP_LOG_LEVEL=debug
    APP_URL=http://localhost/
    
    DB_CONNECTION=mysql
    DB_HOST=DB-KIYOSHIHOST.kiyoshisushi.online
    DB_PORT=3306
    DB_DATABASE=db_kiyoshi
    DB_USERNAME=db_kiyoshiadmin
    DB_PASSWORD=kiyoshipw
    
    BROADCAST_DRIVER=log
    CACHE_DRIVER=file
    SESSION_DRIVER=file
    QUEUE_DRIVER=sync
    
    REDIS_HOST=127.0.0.1
    REDIS_PASSWORD=null
    REDIS_PORT=6379
    
    MAIL_DRIVER=smtp
    MAIL_HOST=smtp.mailtrap.io
    MAIL_PORT=2525
    MAIL_USERNAME=cb8aa7c88c5e7b
    MAIL_PASSWORD=d0a85b2fe7cacb
    MAIL_ENCRYPTION=null
    
    PUSHER_APP_ID=
    PUSHER_APP_KEY=
    PUSHER_APP_SECRET=
    
    STRIPE_SECRET=123133SFSDF32423

---
+++ resources/views/order.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section ('title')
+  Order Kiyoshi Sushi
+@endsection
+
+@section('content')
+	<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Vegetable_gimbap.jpg/1200px-Vegetable_gimbap.jpg" alt="..." style="max-height: 150px" class="img-responsive">
+      <div class="caption">
+        <h3>Food1</h3>
+        <p>DIS IS DA FEWD</p>
+        <p><a href="#" class="btn btn-default" role="button">Add</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+
