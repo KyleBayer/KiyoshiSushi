c6e2ea21ab7471fd622383db4f22d5c9a61ed788
Kyle Bayer <https://KyleBayer@github.com>
2018-11-19 09:57:31 -0500

    Submiting changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.master')
  
 @section('content')
 <div class="container">
@@ -76,3 +76,5 @@
                 </div>
             </div>
         </div>
+    </div>
+</div>
