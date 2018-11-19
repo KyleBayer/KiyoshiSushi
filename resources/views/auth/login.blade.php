369bc215f95d377f4506995085f58cf6ca8a7d17
Shreeji Patel <shreejibharatkumar.patel@dcmail.ca>
2018-11-19 09:51:27 -0500

    df

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
  
 @section('content')
 <div class="container">
-<<<<<<< HEAD
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -18,41 +18,6 @@
                             <div class="col-md-6">
                                 <input type="email" class="form-control" id="email" name="email" value = "{{ old('email') }}" required autofocus>
 
-                                
-=======
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
- 
-                    @if ($errors->has('email'))
-                        <div class="alert alert-warning">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </div>
-                    @endif
- 
-                    @if(session()->has('error'))
-                        <div class="alert alert-danger">
-                            {{ session()->get('error') }}
-                        </div>
-                    @endif
- 
-                    @if(session()->has('success'))
-                        <div class="alert alert-success">
-                            {{ session()->get('success') }}
-                        </div>
-                    @endif
- 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
- 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
- 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
->>>>>>> 755f57ec6500e67644dade0ad3ce55e6b6d93c01
                             </div>
                         </div>
  
@@ -60,18 +25,9 @@
                             <label for="password" class="col-md-4 control-label">Password</label>
  
                             <div class="col-md-6">
-<<<<<<< HEAD
+
                                 <input type="password" class="form-control" id="password" name="password" required>
 
-=======
-                                <input id="password" type="password" class="form-control" name="password" required>
- 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
->>>>>>> 755f57ec6500e67644dade0ad3ce55e6b6d93c01
                             </div>
                         </div>
  
