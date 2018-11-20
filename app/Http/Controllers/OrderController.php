032efb31a5084fe4f6fca59846b7b2ba7f6ada8d
Waqar Naseer <Waqar.n@outlook.com>
2018-11-20 16:32:08 -0500

    working on orderpage

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use Illuminate\Http\Request;
+
+//use App\User;
+use Illuminate\Support\Facades\Hash;
+use App\Mail\Welcome;
+use App\Http\Requests\RegistrationForm;
+
+
+
+class OrderController extends Controller
+{
+   
+   return view('order');
+}
