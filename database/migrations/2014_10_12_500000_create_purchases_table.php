934fb9e1c3ad75d1f6fcebdcf68cb7d0d161f6bf
Shreeji Patel <shreejibharatkumar.patel@dcmail.ca>
2018-12-05 13:33:18 -0500

    fs

--- database/migrations/2014_10_12_500000_create_purchases_table.php
+++ database/migrations/2014_10_12_500000_create_purchases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePurchases extends Migration
+class CreatePurchasesTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2014_10_12_600000_create_orders_table.php
+++ database/migrations/2014_10_12_600000_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrders extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
