582e3aa35027bbb376a34b12ee06b6df7ae16151
Kyle Bayer <https://KyleBayer@github.com>
2018-12-05 13:09:17 -0500

    test

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,21 +1,32 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section ('title')
   Order Kiyoshi Sushi
 @endsection
 
+<!--https://stackoverflow.com/questions/43090063/how-to-get-data-from-database-to-view-page-in-laravel -->
 @section('content')
 	<div class="row">
   <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
+    @foreach($food as $key => $data)
+      <tr>    
+        <th>{{$data->Meal_Name}}</th>
+        <th>{{$data->Meal_Price}}</th>
+        <th>{{$data->Meal_Desc}}</th>               
+      </tr>
+    @endforeach
+  </div>
+</div>
+@endsection
+
+
+
+
+<!-- <div class="thumbnail">
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Vegetable_gimbap.jpg/1200px-Vegetable_gimbap.jpg" alt="..." style="max-height: 150px" class="img-responsive">
       <div class="caption">
         <h3>Food1</h3>
         <p>DIS IS DA FEWD</p>
         <p><a href="#" class="btn btn-default" role="button">Add</a></p>
       </div>
-    </div>
-  </div>
-</div>
-@endsection
-
+    </div> -->
