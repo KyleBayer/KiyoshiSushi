c72fed47621cb3599bbc1c98cc6665c84f08fa35
Kyle Bayer <https://KyleBayer@github.com>
2018-12-09 00:25:16 -0500

    Added Meal model

---
+++ app/Meal.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Meal extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'Meal_Name', 'Meal_Price', 'Meal_Desc', 'Supplier_Address'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        
+    ];
+}
